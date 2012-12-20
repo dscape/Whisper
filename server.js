ccfebf73b1608b4e4e9c145be9dd01f3fcb2998e
Talon <talon.poole@gmail.com>
2012-12-19 19:38:26 -0700

    security!

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ var express = require('express'),
 	port = 80,
 	server = app.listen(port),
 	io = require('socket.io').listen(server),
-	cardboardbox = 'share briefly';
-	
-app.use(express.static(__dirname + '/html'));
-console.log('Server listening to port ' + port);
+	cardboardbox = 'share briefly', //Default Whisper.
+	sanitize = require('validator').sanitize;
+app.use(express.static(__dirname + '/html')); //Serves static files from the HTML folder
 console.log('');
+console.log('Server listening to port ' + port);
 console.log('Press "Ctrl + C" to terminate the server.');
 
 io.on('connection', function(client){
 	setTimeout(function(){
 			client.send(cardboardbox);
 		}
-	,1000);
-	client.on('message', function(whisper){
+	,1000); //Branding!
+	client.on('message', function(dirtywhisper){
+		var whisper=sanitize(dirtywhisper).entityEncode(); //Squeaky clean!
 		cardboardbox = whisper;
 		client.send(whisper);
 		client.broadcast.send(whisper);
